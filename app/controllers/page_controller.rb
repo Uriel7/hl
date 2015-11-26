5d1ca54d1b6e4d3c36e3d9b1a19805f9b82c826d
pepeuriel <j.urielsz@gmail.com>
2015-11-26 12:45:11 -0600

    First Commit

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,19 @@
+class PageController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def business
+  end
+
+  def products
+  end
+
+  def questions
+  end
+
+  def contact
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+
+  get 'page/home'
+
+  get 'page/about'
+
+  get 'page/business'
+
+  get 'page/products'
+
+  get 'page/questions'
+
+  get 'page/contact'
+
+end
